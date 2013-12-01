0c5b3bd37fedce6f1a4d294e9254d716ff47a130
Eric Huppert <eric@erichuppert.com>
2013-12-01 16:19:33 -0500

    cleaned up small js error

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -4,33 +4,30 @@ function initialize() {
 
 	autocomplete = new google.maps.places.Autocomplete(query, options);
 	google.maps.event.addListener(autocomplete, 'place_changed', function(){
-		fill();
-		set_address(street_num, street_name);
+		place = autocomplete.getPlace();
+
+		for (var i = 0; i < place.address_components.length; i++){
+			if (place.address_components[i].types.indexOf('street_number') > -1){
+				street_num = place.address_components[i]['long_name'];
+			};
+
+			if (place.address_components[i].types.indexOf('route') > -1){
+				street_name = place.address_components[i]['long_name'];
+			};
+
+			if (place.address_components[i].types.indexOf('locality') > -1){
+				document.getElementById('address_city').value = place.address_components[i]['long_name'];
+			};
+
+			if (place.address_components[i].types.indexOf('administrative_area_level_1') > -1){
+				document.getElementById('address_state').value = place.address_components[i]['long_name'];
+			};
+		};
+		window.setTimeout(function(){
+			document.getElementById('address_address').value = street_num + " " + street_name;
+		});
 	});
 }
-
-function fill() {
-	place = autocomplete.getPlace();
-
-	for (var i = 0; i < place.address_components.length; i++){
-		if (place.address_components[i].types.indexOf('street_number') > -1){
-			document.getElementById('address_address').value = place.address_components[i]['long_name'] + " ";
-		};
-
-		if (place.address_components[i].types.indexOf('route') > -1){
-			document.getElementById('address_address').value += place.address_components[i]['long_name'];
-		};
-
-		if (place.address_components[i].types.indexOf('locality') > -1){
-			document.getElementById('address_city').value = place.address_components[i]['long_name'];
-		};
-
-		if (place.address_components[i].types.indexOf('administrative_area_level_1') > -1){
-			document.getElementById('address_state').value = place.address_components[i]['long_name'];
-		};
-	};
-}
-
 
 window.onload = function(){
 	initialize();
