454b86131db0ed52e8be40383be9eb5ec5415aa1
Eric Huppert <eric@erichuppert.com>
2013-12-01 16:30:30 -0500

    variable definition consistency

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -2,17 +2,17 @@ function initialize() {
 	var query = document.getElementById('address_address');
 	var options = {componentRestrictions: {country: 'us'}};
 
-	autocomplete = new google.maps.places.Autocomplete(query, options);
+	var autocomplete = new google.maps.places.Autocomplete(query, options);
 	google.maps.event.addListener(autocomplete, 'place_changed', function(){
-		place = autocomplete.getPlace();
+		var place = autocomplete.getPlace();
 
 		for (var i = 0; i < place.address_components.length; i++){
 			if (place.address_components[i].types.indexOf('street_number') > -1){
-				street_num = place.address_components[i]['long_name'];
+				var street_num = place.address_components[i]['long_name'];
 			};
 
 			if (place.address_components[i].types.indexOf('route') > -1){
-				street_name = place.address_components[i]['long_name'];
+				var street_name = place.address_components[i]['long_name'];
 			};
 
 			if (place.address_components[i].types.indexOf('locality') > -1){
